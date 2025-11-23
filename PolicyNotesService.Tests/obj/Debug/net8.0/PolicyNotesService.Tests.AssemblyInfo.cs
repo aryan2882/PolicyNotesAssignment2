b3c9ac90f04652e21a6afb0424d70f713917cd0b
aryan <aryan.raj2022@vitstudent.ac.in>
2025-11-23 18:10:24 +0530

    Initial commit with source code only

--- PolicyNotesService.Tests/obj/Debug/net8.0/PolicyNotesService.Tests.AssemblyInfo.cs
+++ PolicyNotesService.Tests/obj/Debug/net8.0/PolicyNotesService.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PolicyNotesService.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090b025950414aa2e5ba312e172ce54288e73bfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PolicyNotesService.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PolicyNotesService.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
